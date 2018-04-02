c2dba7c2836734437aca4bde07699888df706e43
Zhaosheng Pan <brglng@gmail.com>
2018-04-02 20:42:49 +0800

    Reduce memory usage by writing the queue to disk file

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
-use std::collections::VecDeque;
 use std::env;
 use std::fs;
 use std::path::PathBuf;
+mod path_queue;
+use path_queue::PathQueue;
 
-fn walk_dir(root_dir: String) {
-    let mut q = VecDeque::new();
+fn walk_dir(root: String) {
+    let mut q = PathQueue::new().unwrap();
 
-    q.push_back(PathBuf::from(root_dir));
+    q.push(PathBuf::from(root)).unwrap();
 
-    while let Some(file) = q.pop_front() {
+    while let Some(file) = q.pop().unwrap() {
+        if file.as_os_str().len() == 0 {
+            break;
+        }
+
         if file.is_dir() {
             let _ = fs::read_dir(&file).map(|entries| {
                 for entry in entries {
                     let _ = entry.map(|entry| {
                         println!("{}", &entry.path().display());
                         if entry.path().is_dir() {
-                            q.push_back(entry.path());
+                            q.push(entry.path()).unwrap();
                         }
                     }).map_err(|err| {
                         eprintln!("bfind: {}", err);
@@ -33,14 +38,50 @@ fn walk_dir(root_dir: String) {
     }
 }
 
+#[allow(dead_code)]
+fn dls(root: PathBuf, depth: usize) -> usize {
+    let mut num_all_children: usize = 0;
+
+    if depth == 0 {
+        num_all_children += 1;
+        println!("{}", &root.display());
+    } else {
+        if root.is_dir() {
+            let _ = fs::read_dir(&root).map(|entries| {
+                for entry in entries {
+                    let _ = entry.map(|entry| {
+                        num_all_children += dls(entry.path(), depth - 1);
+                    }).map_err(|err| {
+                        eprintln!("bfind: {}", &err);
+                    });
+                }
+            }).map_err(|err| {
+                eprintln!("bfind: {}: {}", &root.display(), &err);
+            });
+        }
+    }
+
+    return num_all_children;
+}
+
+#[allow(dead_code)]
+fn iddfs(root: String, min_depth: usize, max_depth: usize) {
+    for i in min_depth..max_depth {
+        if dls(PathBuf::from(&root), i) == 0 {
+            break;
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let root_dir = if args.len() > 1 {
+    let root = if args.len() > 1 {
         args[1].clone()
     } else {
         String::from(".")
     };
 
-    walk_dir(root_dir);
+    walk_dir(root);
+    // iddfs(root, 0, 50);
 }
