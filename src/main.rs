b959373b872cb394f29e9d1ee59bd2e40178cd7a
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2024-07-24 16:12:15 +0800

    feat!: use iterative deepening breadth-first travasal instead of tempfile

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,57 @@
 use std::env;
 use std::fs;
-use std::path::PathBuf;
+use std::os::unix::ffi::OsStrExt;
+use std::path::Path;
 use std::process::exit;
 
-mod path_queue;
-use path_queue::PathQueue;
+fn depth_first_traverse(prog: &str, root: &Path, allow_dot: bool, follow_links: bool, iter_depth: i32, depth: i32) {
+    let entries = fs::read_dir(root);
+    if let Ok(entries) = entries {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let path = entry.path();
 
-fn walk_dir(prog: &str, root: &str, allow_dot: bool, follow_links: bool) {
-    let mut q = PathQueue::new().unwrap();
+                let is_link = path.read_link().is_ok();
+                let mut is_dot = false;
 
-    q.push(PathBuf::from(root)).unwrap();
+                let file_name = path.file_name();
+                if let Some(file_name) = file_name {
+                    let file_name_bytes = file_name.as_bytes();
+                    if !file_name_bytes.is_empty() && file_name_bytes[0] == b'.' {
+                        is_dot = true;
+                    }
+                } else {
+                    eprintln!("{}: {}: cannot get filename", prog, path.display());
+                    continue;
+                }
 
-    while let Some(file) = q.pop().unwrap() {
-        let entries = fs::read_dir(&file);
-        if let Ok(entries) = entries {
-            for entry in entries {
-                if let Ok(entry) = entry {
-                    let path = entry.path();
-
-                    let is_link = path.read_link().is_ok();
-                    let mut is_dot = false;
-
-                    let file_name = path.file_name();
-                    if let Some(file_name) = file_name {
-                        let file_name_str = file_name.to_str();
-                        if let Some(file_name_str) = file_name_str {
-                            if file_name_str.starts_with(".") {
-                                is_dot = true;
-                            }
-                        } else {
-                            eprintln!("{}: {}: bad filename", prog, path.display());
-                            continue;
+                if (follow_links || !is_link) && (allow_dot || !is_dot) {
+                    if depth < iter_depth {
+                        if path.is_dir() {
+                            depth_first_traverse(prog, &entry.path(), allow_dot, follow_links, iter_depth, depth + 1);
                         }
                     } else {
-                        eprintln!("{}: {}: cannot get filename", prog, path.display());
-                        continue;
+                        println!("(depth = {}, iter_depth = {}) {}", depth, iter_depth, path.display());
                     }
-
-                    if (follow_links || !is_link) && (allow_dot || !is_dot) {
-                        println!("{}", path.display());
-                        if path.is_dir() {
-                            q.push(path).unwrap();
-                        }
-                    }
-                } else if let Err(e) = entry {
-                    eprintln!("{}: {}: {}", prog, file.display(), e);
-                };
+                }
+            } else if let Err(e) = entry {
+                eprintln!("{}: {}: {}", prog, root.display(), e);
+                continue;
             }
-        } else if let Err(e) = entries {
-            eprintln!("{}: {}: {}", prog, file.display(), e);
         }
+    } else if let Err(e) = entries {
+        eprintln!("{}: {}: {}", prog, root.display(), e);
+    }
+}
+
+fn iterative_deepening(prog: &str, root: &Path, allow_dot: bool, follow_links: bool, max_depth: i32) {
+    for depth in 1..=max_depth {
+        depth_first_traverse(prog, root, allow_dot, follow_links, depth, 1);
     }
 }
 
 fn print_help(prog: &str) {
-    println!("{}: [-H] [-L] [-P] [-D debugopts] [-Olevel] [starting-point...] [expression]", prog);
+    println!("{}: [-H] [-L] [starting-point...] [expression]", prog);
     exit(0);
 }
 
@@ -74,14 +71,14 @@ fn main() {
         } else if arg == "-L" {
             follow_links = true;
         } else if arg == "-h" || arg == "--help" {
-            print_help(&prog);
-        } else if arg.len() > 1 && arg.starts_with("-") {
+            print_help(prog);
+        } else if arg.len() > 1 && arg.starts_with('-') {
             eprintln!("{}: unknown argument: {}", &prog, &arg);
             exit(-1);
         } else {
-            root = arg.clone();
+            root.clone_from(&arg);
         }
     }
 
-    walk_dir(prog, &root, allow_dot, follow_links);
+    iterative_deepening(prog, Path::new(&root), allow_dot, follow_links, 100);
 }
